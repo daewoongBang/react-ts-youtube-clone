0bef307cfbad435890f665820811af6880f83430
Daewoong Bang <59325523+daewoongBang@users.noreply.github.com>
2025-04-03 22:35:50 +0900

    style: apply styles to search header

--- src/components/layout/SearchHeader.tsx
+++ src/components/layout/SearchHeader.tsx
@@ -32,21 +32,21 @@ const SearchHeader = () => {
   }, [pathname, searchParams]);
 
   return (
-    <header>
-      <div>
-        <Link to='/'>
-          <BsYoutube /> <h1>YouTube</h1>
-        </Link>
-      </div>
+    <header className='w-full flex p-4 text-2xl border-b border-zinc-600 mb-4'>
+      <Link to='/' className='flex items-center'>
+        <BsYoutube className='text-4xl text-brand' />
+        <h1 className='font-bold ml-2 text-3xl'>YouTube</h1>
+      </Link>
 
-      <form onSubmit={handleSubmit}>
+      <form className='w-full flex justify-center' onSubmit={handleSubmit}>
         <input
+          className='w-7/12 p-2 outline-none bg-black text-gray-50'
           type='text'
           placeholder='Search...'
           value={searchText}
           onChange={handleChange}
         />
-        <button type='submit'>
+        <button className='bg-zinc-600 px-4' type='submit'>
           <BsSearch />
         </button>
       </form>
